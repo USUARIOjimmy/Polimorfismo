2c34b9d9f47539d7f39dd788c7f205530559c8ab
USUARIOjimmy <102883317+USUARIOjimmy@users.noreply.github.com>
2022-04-06 23:37:22 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿
+using System;
+
+namespace ejercicio1SobrecargaOperadores{
+	class Program{
+		public static void Main(string[] args){
+			
+			
+			Vector v1=new Vector();
+			Vector v2=new Vector();
+			//a) ++ para mostrar datos
+			Console.WriteLine("----- inciso a -----");
+			//v1++;
+			v2++;
+			//b) -- para leer datos
+			Console.WriteLine("----- inciso b -----");
+			v2--;
+			//v1--;
+			//c) + para incrementar los datos en k unidades
+			Console.WriteLine("----- inciso c -----");
+			
+			Console.Write("ingrese k: ");
+			int k = int.Parse(Console.ReadLine());
+			v2 = v2 + k;
+			v2++;
+			//v1 = v1 + k;
+			//v1++;
+			//d) * dados dos objetos vector mostrar los datos de aquel que tiene mas elementos
+			Console.WriteLine("----- inciso d -----");
+			//v1 = v1 > v2; //error el metodo > es bool (v1 = v1 > v2 si funciona si es  Vector )
+			bool sw=v1*v2;
+			if(sw){
+				Console.WriteLine("mayor es:");
+				v1++;
+			}
+			else{
+				Console.WriteLine("mayor es:");
+				v2++;
+			}
+			//e) % para determinar cuantas veces se repite el dato k
+			Console.WriteLine("----- inciso d -----");
+			Console.Write("ingrese k: ");
+			int k2 = int.Parse(Console.ReadLine());
+			int salida=v1 % k2;
+			Console.WriteLine("se repite: "+salida);
+			//int salida2=v2 % k2;
+			//Console.WriteLine("se repite: "+salida2);
+			
+			/*
+			----- inciso a -----
+			n: 3
+			23
+			45
+			45
+			----- inciso b -----
+			ingrese n: 1
+			ingrese vn: 22
+			----- inciso c -----
+			ingrese k: 1
+			n: 1
+			23
+			----- inciso d -----
+			mayor es:
+			n: 3
+			23
+			45
+			45
+			----- inciso d -----
+			ingrese k: 45
+			se repite: 2
+			 */
+
+			
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ Vector.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+namespace ejercicio1SobrecargaOperadores{
+	public class Vector{
+		//00000000000000 nota 000000000000
+		//Sobrecarga de Operadores no hay  this
+		//ATRIBUTOS
+		private int n;
+		private int[] v=new int[50];
+		//METODOS
+		//CONSTRUCTOR
+		public Vector(){
+			this.n=3;
+			this.v[0]=23;
+			this.v[1]=45;
+			this.v[2]=45;
+			
+		}
+		//a) ++ para mostrar datos
+		public static Vector operator ++(Vector v1){
+			Console.WriteLine("n: "+v1.n);
+			for(int i=0;i<v1.n;i++){
+				Console.WriteLine(v1.v[i]);
+			}
+			return v1;
+		}
+		//b) - - para leer datos
+		public static Vector operator --(Vector v1){
+			Console.Write("ingrese n: ");
+			v1.n=int.Parse(Console.ReadLine());
+			for(int i=0;i<v1.n;i++){
+				Console.Write("ingrese vn: ");
+				v1.v[i]=int.Parse(Console.ReadLine());
+			}
+			return v1;
+		}
+		//c) + para incrementar los datos en k unidades
+		public static Vector operator +(Vector v1 , int k){
+			for( int i=0;i<v1.n;i++){
+				v1.v[i]=v1.v[i]+k;
+				
+			}
+			return v1;
+		}
+		//d) < dados dos objetos vector mostrar los datos de aquel que tiene mas elementos
+		public static bool operator * (Vector v1, Vector v2){
+			if(v1.n > v2.n){
+				return true;
+			}
+			return false;
+		}
+		//e) % para determinar cuantas veces se repite el dato k
+		public static int operator % (Vector v1,int k2){
+			int contador=0;
+			for(int i=0;i<v1.n;i++){
+				if(v1.v[i] == k2){
+					contador=contador+1; //constador++;
+				}
+			}
+			return contador;
+		}
+		
+
+	}
+}
